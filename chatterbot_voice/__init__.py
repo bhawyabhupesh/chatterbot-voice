813500f45157dd2dc6c2dca8c65006d2af10eb4c
Gunther Cox <gunthercx@gmail.com>
2017-10-08 11:08:05 -0400

    Update version to 0.1.5

--- chatterbot_voice/__init__.py
+++ chatterbot_voice/__init__.py
@@ -4,7 +4,7 @@ if 'install' not in sys.argv and 'egg_info' not in sys.argv:
     from .voice import VoiceInput
     from .voice import VoiceOutput
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 __all__ = (
